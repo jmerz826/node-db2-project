0c7d08cfc876a2040dc312ee3ae2ebe5bc07e88a
John Merz <jmerz826@gmail.com>
2022-01-18 21:24:42 -0500

    migrations and seed built

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,6 +1,12 @@
 exports.up = async function (knex) {
   await knex.schema.createTable('cars', table => {
     table.increments() // primary key called 'id' using auto incrm. integers
+    table.text('vin', 128).unique().notNullable()
+    table.text('make', 128).notNullable()
+    table.text('model', 128).notNullable()
+    table.float('mileage').notNullable()
+    table.text('title', 128)
+    table.text('transmission', 128)
   })
 };
 
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,10 @@
 // STRETCH
+
+exports.seed = async function (knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert([
+        { vin: '2HASWIJV48HJASFGJ', make: 'Jeep', model: 'Liberty', mileage: 8500, title: 'John', transmission: 'automatic' },
+        { vin: '45TJASG94JMGAWERT', make: 'Nisssan', model: 'Rogue', mileage: 3805, title: 'Dennis' },
+        {vin:  'AW4ET8GAN3WG8HH8Q', make: 'Jeep', model: 'Compass', mileage: 28513, title: 'Phil', transmission: 'manual'}
+    ])
+}
